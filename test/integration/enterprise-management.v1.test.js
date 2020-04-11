aec92ab27b3a7e6c31ab01958aa475af1dff152a
Phil Adams <phil_adams@us.ibm.com>
2020-04-11 07:59:35 -0500

    test: update ent-mgmt int test with additional methods

--- test/integration/enterprise-management.v1.test.js
+++ test/integration/enterprise-management.v1.test.js
@@ -30,15 +30,228 @@ const configFile = 'enterprise-management.env';
 // Use authHelper to skip tests if our configFile is not available.
 const describe = authHelper.prepareTests(configFile);
 
-describe('IamAccessGroupsV2_integration', () => {
+const parent = 'crn:v1:bluemix:public:enterprise::a/74bcc349d963484f82e8279ba310c6df::enterprise:cbbb066a123a40ebadb806a293655aec';
+const name = 'IBM-1586402147632';
+const primaryContactIamId = 'IBMid-550006CNQ6';
+const limit = 100;
+const enterpriseId = '1b1d66e78e5941708e5775728ab22c51';
+const parentAccountGroupId = '7e96629680604e0b9de15712c84719eb';
+const accountId = 'b8a7c19fa050430b997c20c636bead83';
+
+describe('EnterpriseManagementV1_integration', () => {
   jest.setTimeout(timeout);
 
   let service;
 
-  test('should successfully complete initialization', done => {
-    // Initialize the service client.
-    service = EnterpriseManagementV1.newInstance();
+  it('should successfully complete initialization', done => {
+    service = EnterpriseManagementV1.newInstance({});
     expect(service).not.toBeNull();
     done();
+  });
+
+  it('should create an account group', done => {
+    const params = {
+      parent: parent,
+      name: name,
+      primaryContactIamId: primaryContactIamId,
+    };
+    return service
+      .createAccountGroup(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(201);
+        done();
+      })
+      .catch(err => {
+        console.log('createAccountGroup error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get account groups by query parameter', done => {
+    const params = {
+      enterpriseId: enterpriseId,
+      parentAccountGroupId: parentAccountGroupId,
+      parent: parent,
+      limit: limit,
+    };
+    service
+      .listAccountGroups(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('listAccountGroups error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get account group by id', done => {
+    const params = {
+      accountGroupId: parentAccountGroupId,
+    };
+    return service
+      .getAccountGroupById(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('getAccountGroupById error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should update an account group', done => {
+    const params = {
+      name: name,
+      primaryContactIamId: primaryContactIamId,
+      accountGroupId: parentAccountGroupId,
+    };
+    return service
+      .updateAccountGroup(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(204);
+        done();
+      })
+      .catch(err => {
+        console.log('updateAccountGroup error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get permissible actions for an account group.', done => {
+    const params = {
+      actions: ['testString'],
+      accountGroupId: parentAccountGroupId,
+    };
+    return service
+      .getAccountGroupPermissibleActions(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('getAccountGroupPermissibleActions error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should import an account into an enterprise', done => {
+    const params = {
+      parent: parent,
+      billingUnitId: 'testString',
+      enterpriseId: enterpriseId,
+      accountId: accountId,
+    };
+    return service
+      .importAccountToEnterprise(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(202);
+        done();
+      })
+      .catch(err => {
+        console.log('importAccountToEnterprise error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should create a new account in an enterprise', done => {
+    const params = {
+      parent: parent,
+      name: name,
+      ownerIamId: primaryContactIamId,
+    };
+    return service
+      .createAccount(params)
+      .then(response => {
+	console.log("createAccount response: ", JSON.stringify(response, null, 4));
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(201);
+        done();
+      })
+      .catch(err => {
+        console.log('createAccount error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get accounts by query parameter', done => {
+    const params = {
+      enterpriseId: enterpriseId,
+      accountGroupId: parentAccountGroupId,
+      parent: parent,
+      limit: limit,
+    };
+    return service
+      .listAccounts(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('listAccounts (by query param) error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get account by id', done => {
+    const params = {
+      accountId: accountId,
+    };
+    return service
+      .getAccountById(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('getAccountById error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should move an account with the enterprise', done => {
+    const params = {
+      parent: parent,
+      accountId: accountId,
+    };
+    return service
+      .updateAccount(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(204);
+        done();
+      })
+      .catch(err => {
+        console.log('updateAccount error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
+  });
+
+  it('should get permissible actions for an account', done => {
+    const params = {
+      actions: ['testString'],
+      accountId: accountId,
+    };
+    return service
+      .getAccountPermissibleActions(params)
+      .then(response => {
+        expect(response.hasOwnProperty('status')).toBe(true);
+        expect(response.status).toBe(200);
+        done();
+      })
+      .catch(err => {
+        console.log('getAccountPermissibleActions error: ', JSON.stringify(err, null, 4));
+        done(err);
+      });
   });
 });
