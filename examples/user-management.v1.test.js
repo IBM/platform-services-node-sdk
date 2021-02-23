7ad49c8cc14722b9d07f5f938fe17458da2791fc
Norbert Biczo <pyrooka@users.noreply.github.com>
2021-02-23 19:20:11 +0100

    fix(User Management): add examples

---
+++ examples/user-management.v1.test.js
@@ -0,0 +1,235 @@
+/**
+* @jest-environment node
+*/
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const UserManagementV1 = require('../dist/user-management/v1');
+const { readExternalSources } = require('ibm-cloud-sdk-core');
+const authHelper = require('../test/resources/auth-helper.js');
+
+// Location of our config file.
+const configFile = 'user_management_v1.env';
+
+const describe = authHelper.prepareTests(configFile);
+
+// Save original console.log and console.warn
+const originalLog = console.log;
+const originalWarn = console.warn;
+
+// Mocks for console.log and console.warn
+const consoleLogMock = jest.spyOn(console, 'log');
+const consoleWarnMock = jest.spyOn(console, 'warn');
+
+describe('UserManagementV1', () => {
+
+  // begin-common
+
+  const userManagementService = UserManagementV1.newInstance({});
+
+  // end-common
+
+  const config = readExternalSources(UserManagementV1.DEFAULT_SERVICE_NAME);
+
+  test('listUsers request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-list_users
+
+    const params = {
+      accountId: 'testString',
+    };
+
+    userManagementService.listUsers(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-list_users
+  });
+  test('inviteUsers request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-invite_users
+
+    const params = {
+      accountId: 'testString',
+    };
+
+    userManagementService.inviteUsers(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-invite_users
+  });
+  test('getUserProfile request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-get_user_profile
+
+    const params = {
+      accountId: 'testString',
+      iamId: 'testString',
+    };
+
+    userManagementService.getUserProfile(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-get_user_profile
+  });
+  test('updateUserProfile request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-update_user_profile
+
+    const params = {
+      accountId: 'testString',
+      iamId: 'testString',
+    };
+
+    userManagementService.updateUserProfile(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-update_user_profile
+  });
+  test('getUserSettings request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-get_user_settings
+
+    const params = {
+      accountId: 'testString',
+      iamId: 'testString',
+    };
+
+    userManagementService.getUserSettings(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-get_user_settings
+  });
+  test('updateUserSettings request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-update_user_settings
+
+    const params = {
+      accountId: 'testString',
+      iamId: 'testString',
+    };
+
+    userManagementService.updateUserSettings(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-update_user_settings
+  });
+  test('removeUser request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-remove_user
+
+    const params = {
+      accountId: 'testString',
+      iamId: 'testString',
+    };
+
+    userManagementService.removeUser(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-remove_user
+  });
+});
