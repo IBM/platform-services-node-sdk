72aa385111663f171a6e0aef1cc5baa84585593b
András Csányi <andras.csanyi@pm.me>
2021-03-09 20:19:15 +0100

    feat(Posture Management): add generated examples of Posture Management service

---
+++ examples/posture-management.v1.test.js
@@ -0,0 +1,138 @@
+/**
+* @jest-environment node
+*/
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const PostureManagementV1 = require('../dist/posture-management/v1');
+const { readExternalSources } = require('ibm-cloud-sdk-core');
+const authHelper = require('../test/resources/auth-helper.js');
+
+//
+// This file provides an example of how to use the Posture Management service.
+//
+// The following configuration properties are assumed to be defined:
+// POSTURE_MANAGEMENT_URL=<service base url>
+// POSTURE_MANAGEMENT_AUTH_TYPE=iam
+// POSTURE_MANAGEMENT_APIKEY=<IAM apikey>
+// POSTURE_MANAGEMENT_AUTH_URL=<IAM token service base URL - omit this if using the production environment>
+//
+// These configuration properties can be exported as environment variables, or stored
+// in a configuration file and then:
+// export IBM_CREDENTIALS_FILE=<name of configuration file>
+//
+const configFile = 'posture_management_v1.env';
+
+const describe = authHelper.prepareTests(configFile);
+
+// Save original console.log and console.warn
+const originalLog = console.log;
+const originalWarn = console.warn;
+
+// Mocks for console.log and console.warn
+const consoleLogMock = jest.spyOn(console, 'log');
+const consoleWarnMock = jest.spyOn(console, 'warn');
+
+describe('PostureManagementV1', () => {
+
+  // begin-common
+
+  const postureManagementService = PostureManagementV1.newInstance({});
+
+  // end-common
+
+  const config = readExternalSources(PostureManagementV1.DEFAULT_SERVICE_NAME);
+
+  test('createValidationScan request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-create_validation_scan
+
+    const params = {
+      accountId: 'testString',
+    };
+
+    postureManagementService.createValidationScan(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-create_validation_scan
+  });
+  test('listProfile request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-list_profile
+
+    const params = {
+      accountId: 'testString',
+    };
+
+    postureManagementService.listProfile(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-list_profile
+  });
+  test('listScopes request example', done => {
+
+    consoleLogMock.mockImplementation(output => {
+      originalLog(output);
+      done();
+    });
+    consoleWarnMock.mockImplementation(output => {
+      done(output);
+    });
+
+    // begin-list_scopes
+
+    const params = {
+      accountId: 'testString',
+    };
+
+    postureManagementService.listScopes(params)
+      .then(res => {
+        console.log(JSON.stringify(res.result, null, 2));
+      })
+      .catch(err => {
+        console.warn(err)
+      });
+
+    // end-list_scopes
+  });
+});
